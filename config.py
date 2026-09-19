4b1b4aeb6f2435259914bff2594f00fcb96d7da3
ktawok <maximfedor38@gmail.com>
2026-09-19 19:40:15 +0300

    config.py

--- config.py
+++ config.py
@@ -1,10 +1,13 @@
-# config.py
 import os
 
+# Считываем токен из секретных переменных Render
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-# Список Telegram ID главных админов (Ты и твои доверенные лица)
+# Список Telegram ID главных админов
 SUPER_ADMINS = [1788867885, "1788867885"]
+
+# Список редакторов (если нужно добавить кого-то из одногруппников)
+EDITORS = []
 
 # Ссылка на твой GitHub Pages (со слэшем на конце)
 WEBAPP_URL = "https://ktawok.github.io/tdr26-mini-app/"
@@ -12,3 +15,6 @@ WEBAPP_URL = "https://ktawok.github.io/tdr26-mini-app/"
 # Группа и ВУЗ
 GROUP_NAME = "ТДР-26"
 UNIVERSITY = "МГРИ"
+
+# Файл базы данных
+DB_NAME = "bot_database.db"
